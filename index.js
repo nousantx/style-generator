5984a33f8dbf41af21ba90992205890f793dd0ee
nousantx <nousantx@gmail.com>
2024-12-09 20:51:46 +0700

    Update index file

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-import { Window } from 'happy-dom'
-import { MakeTenoxUI } from '@tenoxui/core'
+import { applyStyles } from './src/applyStyles.js'
 
-const apply = (element) => {
-  const window = new Window()
-  const document = window.document
-
-  const temp = document.createElement('div')
-  temp.innerHTML = element
-
-  temp.querySelectorAll('*').forEach((el) => {
-    const styler = new MakeTenoxUI({
-      element: el,
-      property: {
-        bg: 'backgroundColor',
-        text: 'color'
-      },
-      aliases: {
-        tx: 'bg-blue text-red'
-      }
-    })
-
-    el.classList.forEach((className) => {
-      styler.applyStyles(className)
-    })
-
-    console.log(el.style.cssText)
-  })
-
-  return temp.innerHTML
-}
-
-const elem = apply('<div class="bg-red text-blue"></div>')
+const elem = applyStyles('<div class="bg-red text-blue"></div>')
 
 console.log(elem)
